65de4179dfbadccf8698ebc2d56394ebe0f536ef
JuanGiacalone <juanpablogiacalone8@gmail.com>
2020-10-14 20:45:21 -0300

    Compilando OK

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,7 +2,7 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` for hints :)
 
-// I AM NOT DONE
+
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
@@ -13,6 +13,11 @@ fn main() {
     }
 }
 
-fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
+fn is_a_color_word(attempt: String) -> bool {
+    if attempt == "green" || attempt == "blue" || attempt == "red"
+    {
+        return true;
+    }
+
+    return false;
 }
